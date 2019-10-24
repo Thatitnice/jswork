a11e96718250c9e8d01e80ad07b23489fff27b74
Thatitnice <487380346@qq.com>
2019-10-24 11:07:18 +0800

    jswork

---
+++ work7slide/index.js
@@ -0,0 +1,14 @@
+function sort(method){
+    let origin = document.getElementById('origin').value
+    let logs
+    console.log(method)
+    if(method=='bubble'){
+        logs = bubbleSort(origin)
+    }else if(method=='insert'){
+        logs = insertSort(origin)
+    }
+    showDiv = document.getElementById('Anim')
+    logValues = los.value()
+    console.log(logValues.next())
+    setTimeout("showLog(logValues,showDiv)",50)
+}
